6db0ab2f739212ba05f8d91f7ac6334be9d2f272
Steven Hoogeveen <sth@vtti.com>
2025-04-05 14:26:53 +0200

    feat(hyundai): adjust lateral tuning parameters and actuator delay
    Updated steerActuatorDelay to 0.2 for improved response. Initialized lateral tuning with 'pid' and refined breakpoint values and gains (kpBP, kiBP, kpV, kiV). Enhances vehicle speed-based steering behavior and tuning flexibility.

--- opendbc_repo/opendbc/car/hyundai/interface.py
+++ opendbc_repo/opendbc/car/hyundai/interface.py
@@ -153,17 +153,19 @@ class CarInterface(CarInterfaceBase):
       # https://github.com/commaai/openpilot/wiki/Tuning
 
       #ret.steerRatio = 13.3
-      ret.steerActuatorDelay = 0.15
+      ret.steerActuatorDelay = 0.2
       # ret.steerRateCost = 0.5
 
-      ret.lateralTuning.pid.kpBP = [11.0, 34.0]    # 11 = 39.6 km/h, 34 = 122.4 km/h  (plan to tune at these speeds)
-      ret.lateralTuning.pid.kiBP = [11.0, 34.0]    # 11 = 39.6 km/h, 34 = 122.4 km/h  (plan to tune at these speeds)
+      ret.lateralTuning.init('pid')
+
+      ret.lateralTuning.pid.kpBP = [11., 34.]    # 11 = 39.6 km/h, 34 = 122.4 km/h  (plan to tune at these speeds)
+      ret.lateralTuning.pid.kiBP = [11., 34.]    # 11 = 39.6 km/h, 34 = 122.4 km/h  (plan to tune at these speeds)
           # kpBP and kiBP are generally identical.
           # The breakpoint units are meters/s and apply to the vehicle speed.
           # Most cars only have two BPs - a low speed and a high speed (41 m/s is about 90 mph for example).
           # The purpose of these tuning arrays is to tweak the proportional and integral gain based on vehicle speed.
 
-      ret.lateralTuning.pid.kpV = [1.0, 1.0]        # 1.0 from PlotJuggler before tuning
+      ret.lateralTuning.pid.kpV = [1., 1.]        # 1.0 from PlotJuggler before tuning
       ret.lateralTuning.pid.kiV = [0.1, 0.1]        # 0.1 from PlotJuggler before tuning
           # kpV and kiV are gain applied to the output of the I and P calculation,
           # which is a scale of 0 to +-1, 0 being no torque, +-1 being 100% of available torque in either direction.
