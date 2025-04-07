eead2510e101151a03d3f30960b02e4b1b72ff8a
Steven Hoogeveen <sth@vtti.com>
2025-04-07 17:06:02 +0200

    feat(hyundai): update steer ratio and PID feedforward tuning
    This commit adjusts the steering ratio to 13.3 and updates the PID feedforward gain from 0.000035 to 0.00004 for improved lateral control performance. These changes aim to enhance vehicle handling precision in Hyundai models.

--- opendbc_repo/opendbc/car/hyundai/interface.py
+++ opendbc_repo/opendbc/car/hyundai/interface.py
@@ -153,8 +153,8 @@ class CarInterface(CarInterfaceBase):
       # https://github.com/commaai/openpilot/wiki/Tuning
 
       ret.steerActuatorDelay = 0.15
+      ret.steerRatio = 13.3
       # ret.steerRateCost = 0.5
-      # ret.steerRatio = 13.3
 
       ret.lateralTuning.init('pid')
 
@@ -173,7 +173,7 @@ class CarInterface(CarInterfaceBase):
           # which is a scale of 0 to +-1, 0 being no torque, +-1 being 100% of available torque in either direction.
           # This is a gross simplification, but should help get the rough idea.
 
-      ret.lateralTuning.pid.kf = 0.000035           # 0.00003 from KisaPilot Tune PID_A
+      ret.lateralTuning.pid.kf = 0.00004            # 0.00003 from KisaPilot Tune PID_A
 
           # Feedforward is the part of the steering controller that only cares about the desired steering angle (how sharp the curve is).
           # So feedforward only comes into play in curves when the desired steering angle is non-zero, and the greater the angle, the greater the feedforward response, which is scaled by kf.
