407da9ad02ff44e263f6c8de5f3b9b2dcc2c43e1
Steven Hoogeveen <github@shoogeveen.nl>
2025-04-19 21:19:39 +0200

    fix(hyundai): adjust lateral PID tuning for improved handling
    Reduced `kpV` value at higher speeds from 0.40 to 0.35 for smoother lane changes and improved stability in aggressive turns. This fine-tunes vehicle dynamics based on feedback and testing.

--- opendbc_repo/opendbc/car/hyundai/interface.py
+++ opendbc_repo/opendbc/car/hyundai/interface.py
@@ -166,7 +166,7 @@ class CarInterface(CarInterfaceBase):
           # Most cars only have two BPs - a low speed and a high speed (41 m/s is about 90 mph for example).
           # The purpose of these tuning arrays is to tweak the proportional and integral gain based on vehicle speed.
 
-      ret.lateralTuning.pid.kpV = [0.55, 0.55, 0.40, 0.15]   # 0.2 from KisaPilot Tune PID_A, 0.6 from KisaPilot Tune_C (more aggressive turns at lower speeds, less aggressive lane change at higher speeds)
+      ret.lateralTuning.pid.kpV = [0.55, 0.55, 0.35, 0.15]   # 0.2 from KisaPilot Tune PID_A, 0.6 from KisaPilot Tune_C (more aggressive turns at lower speeds, less aggressive lane change at higher speeds)
       ret.lateralTuning.pid.kiV = [0.03, 0.03, 0.04, 0.06]   # 0.05 from KisaPilot Tune PID_A, 0.1 from KisaPilot Tune_C (allow for quicker turns at low speed, less overcorrecting at higher speeds)
 
           # kpV and kiV are gain applied to the output of the I and P calculation,
