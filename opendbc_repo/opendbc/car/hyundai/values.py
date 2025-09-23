347750361ed5330e88178a97917c9e78411b3140
Steven Hoogeveen <github@shoogeveen.nl>
2025-09-23 18:02:55 +0200

    Add experimental KIA_NIRO_EV_2ND_GEN tuning and parameter overrides; update NIRO_EV_2ND_GEN car specs and values.

--- opendbc_repo/opendbc/car/hyundai/values.py
+++ opendbc_repo/opendbc/car/hyundai/values.py
@@ -34,6 +34,17 @@ class CarControllerParams:
       self.STEER_DELTA_UP = 2
       self.STEER_DELTA_DOWN = 3
 
+    # Experimental KIA_NIRO_EV_2ND_GEN Params (compared to defaults)
+    elif CP.carFingerprint in (CAR.KIA_NIRO_EV_2ND_GEN):
+      self.STEER_DELTA_UP = 6           # increased 3
+      self.STEER_DELTA_DOWN = 10        # increased 3
+      self.STEER_DRIVER_ALLOWANCE = 120 # increased 70
+      self.STEER_DRIVER_MULTIPLIER = 2  # Unchanged
+      self.STEER_DRIVER_FACTOR = 1      # Unchanged
+      self.STEER_THRESHOLD = 130        # decreased 20
+      self.STEER_MAX = 384              # Unchanged, default for most HKG
+      self.STEER_STEP = 1               # Unchanged, 100 Hz
+
     # To determine the limit for your car, find the maximum value that the stock LKAS will request.
     # If the max stock LKAS request is <384, add your car to this list.
     elif CP.carFingerprint in (CAR.GENESIS_G80, CAR.HYUNDAI_ELANTRA, CAR.HYUNDAI_ELANTRA_GT_I30, CAR.HYUNDAI_IONIQ,
@@ -418,7 +429,7 @@ class CAR(Platforms):
       HyundaiCarDocs("Kia Niro EV (without HDA II) 2023-25", "All", car_parts=CarParts.common([CarHarness.hyundai_a])),
       HyundaiCarDocs("Kia Niro EV (with HDA II) 2025", "Highway Driving Assist II", car_parts=CarParts.common([CarHarness.hyundai_r])),
     ],
-    KIA_NIRO_EV.specs,
+    CarSpecs(mass=1739, wheelbase=2.72, steerRatio=13.3, tireStiffnessFactor=0.385),  # https://www.evspecifications.com/en/comparison/afe35ff1
     flags=HyundaiFlags.EV,
   )
   KIA_NIRO_PHEV = HyundaiPlatformConfig(
